682ed07337326e1e02a317cc898d517c8d41a477
Mohamedzake <zakem7671@gmail.com>
2024-11-16 16:07:08 +0200

    j

--- app/[lang]/cabins/page.tsx
+++ app/[lang]/cabins/page.tsx
@@ -1,27 +1,17 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
-import { useTranslations } from "next-intl";
+// import { useRouter } from "next/navigation";
+// import { Button } from "@/components/ui/button";
+// import { useTranslations } from "next-intl";
 
-export default function HomePage({ params }) {
-  const t = useTranslations();
-  const router = useRouter();
+export default function HomePage() {
+  // const t = useTranslations();
+  // const router = useRouter();
 
-  const toggleLanguage = () => {
-    const newLang = params.lang === "en" ? "ar" : "en";
-    router.push(`/${newLang}/cabins`);
-  };
+  // const toggleLanguage = () => {
+  //   const newLang = params.lang === "en" ? "ar" : "en";
+  //   router.push(`/${newLang}/cabins`);
+  // };
 
-  return (
-    <div>
-      <Button className="mt-4" onClick={toggleLanguage}>
-        {t("Title")}
-      </Button>
-      <p>{t("ourStory")}</p>
-      <a href="/" className="mr-4">
-        {t("backToHome")}
-      </a>
-    </div>
-  );
+  return <div>cabins</div>;
 }
