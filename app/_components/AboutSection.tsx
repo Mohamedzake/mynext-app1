5e89ae7f51e9f5def6f0d3bc0315ea8e30948ae7
Mohamedzake <zakem7671@gmail.com>
2024-11-30 14:21:27 +0200

    j

--- app/_components/AboutSection.tsx
+++ app/_components/AboutSection.tsx
@@ -320,18 +320,18 @@ import React from "react";
 import Image from "next/image";
 import imgbg from "@/public/small-r.jpeg";
 import small from "@/public/mid-r.jpeg";
-
+import small1 from "@/public/small1.png";
 const AboutSection: React.FC = () => {
   return (
-    <section className="relative bg-blue-900 h-auto">
+    <section className="relative bg-primary-30 h-auto">
       {/* Content Section */}
-      <div className="relative mx-auto flex flex-col-reverse lg:flex-row items-center justify-between lg:h-[87vh]">
+      <div className="relative mx-auto flex flex-col-reverse lg:flex-row items-center justify-between lg:h-[78vh]">
         {/* Text Section */}
-        <div className="w-full lg:w-1/2 text-center lg:text-right z-50 text-black flex flex-col justify-center py-10 px-6 sm:px-10 lg:px-24 bg-white lg:rounded-l-full order-2 lg:order-2">
+        <div className="w-full lg:w-1/2 text-center lg:text-right z-50 text-black flex flex-col justify-center lg:py-24 py-10 px-6 sm:px-10  bg-white rounded-b-full lg:rounded-br-none lg:rounded-l-full order-2 lg:order-2">
           <h2 className="text-3xl sm:text-4xl lg:text-6xl font-bold mb-4">
             AI ماي كاش
           </h2>
-          <p className="text-lg sm:text-xl lg:text-2xl mb-8 leading-relaxed">
+          <p className="text-lg sm:text-xl lg:text-1xl mb-6 leading-relaxed">
             أُسست ماي كاش لتلبي الاحتياج الذي تطلبه النمو المتسارع في قطاع
             الأعمال في المملكة. حيث طورنا نظاماً لتحسين طرق إدارة المدفوعات
             وإصدار فواتير للمبيعات بسلاسة. تستخدم ماي كاش أحدث تقنيات الحوسبة
@@ -357,7 +357,7 @@ const AboutSection: React.FC = () => {
               <div className="w-4 h-4 rounded-full bg-green-500"></div>
             </div>
           </div> */}
-          <div className="flex flex-col gap-4 mb-8">
+          <div className="flex flex-col mr-7 lg:mr-0 gap-4 mb-8">
             <div className="flex items-center justify-end gap-4 text-end sm:text-end">
               <p className="text-lg sm:text-xl lg:text-2xl leading-relaxed">
                 AI بالعديد من المميزات المصممة خصيصاً لتسهيل
@@ -378,11 +378,11 @@ const AboutSection: React.FC = () => {
             </div>
           </div>
 
-          <div className="flex flex-col lg:flex-row justify-center lg:justify-end gap-4">
-            <a className="border border-primary-20 text-primary-20 font-bold text-lg px-4 py-2 rounded-full hover:bg-green-50 transition w-full lg:w-auto text-center">
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:justify-end gap-4">
+            <a className="border border-primary-20 text-primary-20 font-bold text-lg px-4 py-2 rounded-full hover:bg-green-50 transition w-56 lg:w-auto text-center">
               اعرف المزيد
             </a>
-            <a className="bg-primary-20 text-lg text-white px-4 py-2 rounded-full hover:bg-green-700 transition w-full lg:w-auto text-center">
+            <a className="bg-primary-20 text-lg text-white px-4 py-2 rounded-full hover:bg-green-700 transition w-56 lg:w-auto text-center">
               احصل على نسختك التجريبية
             </a>
           </div>
@@ -390,18 +390,18 @@ const AboutSection: React.FC = () => {
         {/* Background Image */}
         <div className="w-full bg-primary-30 hidden lg:block lg:w-1/2 h-64 sm:h-80 md:h-96 lg:h-full relative order-1 lg:order-1">
           <Image
-            src={imgbg}
+            src={small1}
             placeholder="blur"
             quality={80}
             layout="fill"
-            objectFit="cover"
+            // objectFit="cover"
             alt="Background Image"
             className="rounded-lg"
           />
         </div>
         <div className="w-full bg-primary-30  block lg:hidden lg:w-1/2 h-64 sm:h-80 md:h-96 lg:h-full relative order-1 lg:order-1">
           <Image
-            src={small}
+            src={small1}
             placeholder="blur"
             quality={80}
             layout="fill"
