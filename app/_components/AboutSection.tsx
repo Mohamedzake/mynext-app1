09af12b25fa8f219cee7430d123ac7aece37b895
Mohamedzake <zakem7671@gmail.com>
2024-12-03 18:15:14 +0200

    k

--- app/_components/AboutSection.tsx
+++ app/_components/AboutSection.tsx
@@ -1,10 +1,13 @@
 // import React from "react";
 // import Image from "next/image";
+// import { useTranslations } from "next-intl";
 // import imgbg from "@/public/small-r.jpeg";
-// import small from "@/public/mid-r.jpeg";
 // import small1 from "@/public/small1.png";
 // import bg from "@/public/bg-half.png";
+
 // function AboutSection() {
+//   const t = useTranslations("aboutSection");
+
 //   return (
 //     <section className="relative bg-primary-30 h-auto" dir="ltr">
 //       <div className="absolute hidden lg:block bg-primary-30 inset-0">
@@ -14,58 +17,36 @@
 //           placeholder="blur"
 //           quality={80}
 //           className="object-cover bg-primary-30"
-//           alt="Mountains and forests with two cabins"
+//           alt={t("bgAlt")}
 //         />
 //       </div>
 //       {/* Content Section */}
 //       <div className="relative mx-auto flex flex-col-reverse lg:flex-row items-center justify-between lg:h-[78vh]">
 //         {/* Text Section */}
-//         <div className="w-full lg:w-1/2 text-center lg:text-right z-50 text-black flex flex-col justify-center lg:py-24 py-10 px-6 sm:px-10  bg-white rounded-b-full lg:rounded-br-none lg:rounded-l-full order-2 lg:order-2">
+//         <div className="w-full lg:w-1/2 text-center lg:text-right z-50 text-black flex flex-col justify-center lg:py-24 py-10 px-6 sm:px-10 bg-white rounded-b-full lg:rounded-br-none lg:rounded-l-full order-2 lg:order-2">
 //           <h2 className="text-3xl sm:text-4xl lg:text-6xl font-bold mb-4">
-//             AI ماي كاش
+//             {t("title")}
 //           </h2>
 //           <p className="text-lg sm:text-xl lg:text-1xl mb-6 leading-relaxed">
-//             أُسست ماي كاش لتلبي الاحتياج الذي تطلبه النمو المتسارع في قطاع
-//             الأعمال في المملكة. حيث طورنا نظاماً لتحسين طرق إدارة المدفوعات
-//             وإصدار فواتير للمبيعات بسلاسة. تستخدم ماي كاش أحدث تقنيات الحوسبة
+//             {t("description")}
 //           </p>
 
-//           {/* <div className="flex flex-col gap-4 mb-8">
-//             <div className="flex items-center gap-4">
-//               <p className="text-lg sm:text-xl lg:text-2xl leading-relaxed">
-//                 AI بالعديد من المميزات المصممة خصيصاً لتسهيل
-//               </p>
-//               <div className="w-4 h-4 rounded-full bg-green-500"></div>
-//             </div>
-//             <div className="flex items-center gap-4">
-//               <p className="text-lg sm:text-xl lg:text-2xl leading-relaxed">
-//                 التعرف على المنتجات تلقائيا والتكامل مع قاعدة البيآنآت
-//               </p>
-//               <div className="w-4 h-4 rounded-full bg-green-500"></div>
-//             </div>
-//             <div className="flex items-center gap-4">
-//               <p className="text-lg sm:text-xl lg:text-2xl font-bold leading-relaxed">
-//                 التحليل الديموغرافي للعملاء وتوقعات
-//               </p>
-//               <div className="w-4 h-4 rounded-full bg-green-500"></div>
-//             </div>
-//           </div> */}
 //           <div className="flex flex-col mr-7 lg:mr-0 gap-4 mb-8">
 //             <div className="flex items-center justify-end gap-4 text-end sm:text-end">
 //               <p className="text-lg sm:text-xl lg:text-2xl leading-relaxed">
-//                 AI بالعديد من المميزات المصممة خصيصاً لتسهيل
+//                 {t("feature1")}
 //               </p>
 //               <div className="w-4 h-4 rounded-full bg-green-500"></div>
 //             </div>
 //             <div className="flex items-center justify-end gap-4 text-end sm:text-start">
 //               <p className="text-lg sm:text-xl lg:text-2xl leading-relaxed">
-//                 التعرف على المنتجات تلقائيا والتكامل مع قاعدة البيآنآت
+//                 {t("feature2")}
 //               </p>
 //               <div className="w-4 h-4 rounded-full bg-green-500"></div>
 //             </div>
 //             <div className="flex items-center justify-end gap-4 text-end sm:text-start">
 //               <p className="text-lg sm:text-xl lg:text-2xl font-bold leading-relaxed">
-//                 التحليل الديموغرافي للعملاء وتوقعات
+//                 {t("feature3")}
 //               </p>
 //               <div className="w-4 h-4 rounded-full bg-green-500"></div>
 //             </div>
@@ -73,24 +54,22 @@
 
 //           <div className="flex flex-col lg:flex-row justify-center items-center lg:justify-end gap-4">
 //             <a className="border border-primary-20 text-primary-20 font-bold text-lg px-4 py-2 rounded-full hover:bg-green-50 transition w-56 lg:w-auto text-center">
-//               اعرف المزيد
+//               {t("learnMore")}
 //             </a>
 //             <a className="bg-primary-20 text-lg text-white px-4 py-2 rounded-full hover:bg-green-700 transition w-56 lg:w-auto text-center">
-//               احصل على نسختك التجريبية
+//               {t("getTrial")}
 //             </a>
 //           </div>
 //         </div>
-//         {/* Background Image */}
-
 //         <div></div>
-//         <div className="w-full bg-primary-30  block lg:hidden lg:w-1/2 h-64 sm:h-80 md:h-96 lg:h-full relative order-1 lg:order-1">
+//         {/* Background Image */}
+//         <div className="w-full bg-primary-30 block lg:hidden lg:w-1/2 h-64 sm:h-80 md:h-96 lg:h-full relative order-1 lg:order-1">
 //           <Image
 //             src={small1}
 //             placeholder="blur"
 //             quality={80}
 //             layout="fill"
-//             // objectFit="cover"
-//             alt="Background Image"
+//             alt={t("bgAlt")}
 //             className="bg-primary-30"
 //           />
 //         </div>
@@ -106,13 +85,16 @@ import { useTranslations } from "next-intl";
 import imgbg from "@/public/small-r.jpeg";
 import small1 from "@/public/small1.png";
 import bg from "@/public/bg-half.png";
+import { useLocale } from "../layout";
 
 function AboutSection() {
+  const { currentLocale } = useLocale();
+  console.log(currentLocale);
   const t = useTranslations("aboutSection");
 
   return (
-    <section className="relative bg-primary-30 h-auto" dir="ltr">
-      <div className="absolute hidden lg:block bg-primary-30 inset-0">
+    <section className="relative bg-primary-30 h-auto">
+      {/* <div className="absolute hidden lg:block bg-primary-30 inset-0">
         <Image
           src={bg}
           layout="fill"
@@ -121,18 +103,39 @@ function AboutSection() {
           className="object-cover bg-primary-30"
           alt={t("bgAlt")}
         />
+      </div> */}
+      <div className="absolute hidden lg:block bg-primary-30 inset-0">
+        <Image
+          src={bg}
+          layout="fill"
+          placeholder="blur"
+          quality={80}
+          className={`object-cover bg-primary-30 ${
+            currentLocale === "en" ? "transform scale-x-[-1]" : ""
+          }`}
+          alt={t("bgAlt")}
+        />
       </div>
+
       {/* Content Section */}
       <div className="relative mx-auto flex flex-col-reverse lg:flex-row items-center justify-between lg:h-[78vh]">
         {/* Text Section */}
-        <div className="w-full lg:w-1/2 text-center lg:text-right z-50 text-black flex flex-col justify-center lg:py-24 py-10 px-6 sm:px-10 bg-white rounded-b-full lg:rounded-br-none lg:rounded-l-full order-2 lg:order-2">
-          <h2 className="text-3xl sm:text-4xl lg:text-6xl font-bold mb-4">
-            {t("title")}
-          </h2>
-          <p className="text-lg sm:text-xl lg:text-1xl mb-6 leading-relaxed">
-            {t("description")}
-          </p>
-
+        <div
+          className={`w-full lg:w-1/2 text-center lg:text-right z-40 text-black flex flex-col justify-center lg:py-24 py-10 px-6 sm:px-10
+         bg-white ${
+           currentLocale === "ar"
+             ? "rounded-b-full lg:rounded-br-none lg:rounded-l-full"
+             : "rounded-b-full lg:rounded-bl-none lg:rounded-r-full"
+         } order-2 lg:order-2`}
+        >
+          <div className="flex flex-col justify-end items-end text-end">
+            <h2 className="text-3xl sm:text-4xl lg:text-6xl font-bold mb-4">
+              {t("title")}
+            </h2>
+            <p className="text-lg sm:text-xl lg:text-1xl mb-6 leading-relaxed">
+              {t("description")}
+            </p>
+          </div>
           <div className="flex flex-col mr-7 lg:mr-0 gap-4 mb-8">
             <div className="flex items-center justify-end gap-4 text-end sm:text-end">
               <p className="text-lg sm:text-xl lg:text-2xl leading-relaxed">
--- app/_components/PromoSection.tsx
+++ app/_components/PromoSection.tsx
@@ -4,19 +4,41 @@ import img from "@/public/Frame 1171278757.png";
 import Image from "next/image";
 import im from "@/public/Frame.png";
 import im1 from "@/public/frame.jpeg";
+import svg from "@/public/svg2-removebg-preview.png";
+import svg1 from "@/public/Get_Started_Right_Image__1_.svg.png";
 const PromoSection: React.FC = () => {
   const t = useTranslations("PromoSection");
 
   return (
-    <section className="relative flex justify-center items-center px-4 sm:px-8 lg:px-20 bg-slate-300 mt-20">
+    <section className="relative flex justify-center items-center px-4 sm:px-8 lg:px-20 bg-slate-50 mt-20">
+      <div className="absolute z-0 bottom-0 right-0">
+        <Image
+          src={svg}
+          // layout="fill"
+          placeholder="blur"
+          quality={80}
+          className="h-80 lg:h-auto "
+          alt="Mountains and forests with two cabins"
+        />
+      </div>
+      <div className="absolute lg:bottom-0 left-0">
+        <Image
+          src={svg1}
+          // layout="fill"
+          placeholder="blur"
+          quality={80}
+          className="h-80 lg:h-auto "
+          alt="Mountains and forests with two cabins"
+        />
+      </div>
       {/* Content Container */}
-      <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between">
+      <div className="container mx-auto flex flex-col lg:flex-row items-center gap-20 lg:gap-8 justify-between">
         {/* Text Section */}
-        <div className="lg:w-1/2 text-center lg:text-end px-4 py-8 order-2 lg:order-1">
+        <div className="lg:w-1/2 text-center z-40 lg:text-end px-4 py-8 order-2 mb-20 lg:order-1">
           <h2 className="text-4xl font-bold text-gray-800 mb-4">
             {t("promoHeading")}
           </h2>
-          <p className="text-2xl text-gray-800">{t("promoDescription")}</p>
+          <p className="text-1xl text-gray-800">{t("promoDescription")}</p>
 
           {/* Call-to-action Button */}
           <button>
@@ -31,7 +53,7 @@ const PromoSection: React.FC = () => {
         </div>
 
         {/* Image Section (Appears First on Small Screens) */}
-        <div className="lg:w-1/2 flex justify-center lg:justify-end order-1 lg:order-2 mt-10 lg:mt-0">
+        <div className="lg:w-1/2 flex justify-center lg:justify-end order-1 lg:order-2">
           <div className="relative">
             {/* Single Image for the two phones */}
             <Image src={img} alt="App Preview" className="max-w-full h-auto" />
@@ -43,3 +65,59 @@ const PromoSection: React.FC = () => {
 };
 
 export default PromoSection;
+// import React from "react";
+// import { useTranslations } from "next-intl";
+// import img from "@/public/Frame 1171278757.png";
+// import Image from "next/image";
+// import im from "@/public/Frame.png";
+// import im1 from "@/public/frame.jpeg";
+// import bg from "@/public/last-img.jpeg";
+// const PromoSection: React.FC = () => {
+//   const t = useTranslations("PromoSection");
+
+//   return (
+//     <section className="relative lg:h-[70vh] flex justify-center items-center px-0 md:px-0 sm:px-0 lg:px-20 bg-slate-300 mt-20">
+//       <div className="  hidden lg:block absolute inset-0">
+//         <Image
+//           src={bg}
+//           layout="fill"
+//           placeholder="blur"
+//           quality={80}
+//           // className="object-cover"
+//           alt="Mountains and forests with two cabins"
+//         />
+//       </div>
+//       {/* Content Container */}
+//       <div className="container mx-auto flex z-40 flex-col lg:flex-row items-center justify-between">
+//         {/* Text Section */}
+//         <div className="lg:w-1/2 text-center lg:text-end px-4 py-8 order-2 lg:order-1">
+//           <h2 className="text-4xl font-bold text-gray-800 mb-4">
+//             {t("promoHeading")}
+//           </h2>
+//           <p className="text-2xl text-gray-800">{t("promoDescription")}</p>
+
+//           {/* Call-to-action Button */}
+//           <button>
+//             <Image
+//               src={im}
+//               alt={t("ctaText")}
+//               height={200}
+//               width={200}
+//               className="mt-10"
+//             />
+//           </button>
+//         </div>
+
+//         {/* Image Section (Appears First on Small Screens) */}
+//         <div className="w-full lg:w-1/2 flex justify-center lg:justify-end order-1 lg:order-2 mt-10 lg:mt-0">
+//           {/* <div></div> */}
+//           <div className="block lg:hidden relative">
+//             <Image src={im1} alt="App Preview" className="max-w-full h-auto" />
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default PromoSection;
